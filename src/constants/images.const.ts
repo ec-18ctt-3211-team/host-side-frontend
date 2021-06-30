32c319a412b0ed78b252f51cb3805798e80a8b12
nhi.ly <nhi.ly@be.com.vn>
2021-07-01 02:00:30 +0700

    feat: add images and create main page

---
+++ src/constants/images.const.ts
@@ -0,0 +1,28 @@
+import { IImageTag } from 'interfaces/image-tag.interface';
+
+export const IMAGES: IImageTag[] = [
+  { name: 'Ho Chi Minh city', src: '/images/locations/hochiminh.jpg' },
+  { name: 'Ha Noi capital', src: '/images/locations/hanoi.jpg' },
+  { name: 'Ha Long Bay', src: '/images/locations/halong.jpg' },
+  { name: 'Da Lat city', src: '/images/locations/dalat.jpg' },
+  { name: 'Da Nang', src: '/images/locations/danang.jpg' },
+  { name: 'Hoi An', src: '/images/locations/hoian.jpg' },
+  { name: 'Hue', src: '/images/locations/hue.jpg' },
+];
+
+export const GIFTS: IImageTag[] = [
+  { src: '/images/gifts/gift1.jpg' },
+  { src: '/images/gifts/gift2.jpg' },
+  { src: '/images/gifts/gift3.jpg' },
+  { src: '/images/gifts/gift4.jpg' },
+  { src: '/images/gifts/gift5.jpg' },
+];
+
+export const ROOMS: IImageTag[] = [
+  { src: '/images/homestays/homestay1.jpg' },
+  { src: '/images/homestays/homestay2.jpg' },
+  { src: '/images/homestays/homestay3.jpg' },
+  { src: '/images/homestays/homestay4.jpg' },
+  { src: '/images/homestays/homestay5.jpg' },
+  { src: '/images/homestays/homestay6.jpg' },
+];

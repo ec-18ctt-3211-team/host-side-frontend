0928644983064840f243bcc318bb04d1ddeb014a
nhi.ly <nhi.ly@be.com.vn>
2021-07-01 00:28:38 +0700

    fix: add images prop in image slider

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import Layout from 'components/layout';
 import ImageSlider from 'components/image-slider/slider';
+import { IMAGES } from 'constants/locations.const';
 
 export default function Main(): JSX.Element {
   const [isAuthorized, setAuthorize] = useState(true);
   return (
     <Layout isAuthorized={isAuthorized}>
-      <ImageSlider title="SIGNIFICANT PLACES TO STAY" limit={5} />
+      <ImageSlider
+        title="SIGNIFICANT PLACES TO STAY"
+        limit={5}
+        images={IMAGES}
+      />
     </Layout>
   );
 }

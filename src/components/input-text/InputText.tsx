cb92f11860fda1a72906622b929d4cfa89a66e74
tcanh20102000 <tcanh18@apcs.vn>
2021-07-03 09:35:01 +0700

    Small changings in styling

--- src/components/input-text/InputText.tsx
+++ src/components/input-text/InputText.tsx
@@ -18,10 +18,8 @@ export const InputTextComponent: React.FC<InputText> = (props) => {
   else {
     return (
       <>
-        {inputPos === 'left' && <InputTextNoBorderLeft {...props} />}
-        {inputPos === 'right' && <InputTextNoBorderRight {...props} />}
-        {inputPos === 'top' && <InputTextNoBorderTop {...props} />}
-        {inputPos === 'bottom' && <InputTextNoBorderBottom {...props} />}
+        {(inputPos === 'left' || inputPos === 'right')  && <InputTextNoBorderLeftRight {...props} />}
+        {(inputPos === 'top' || inputPos === 'bottom') && <InputTextNoBorderTopBot {...props} />}
       </>
     );
   }
@@ -40,66 +38,34 @@ const InputTextBorder: React.FC<InputText> = (props) => {
     </div>
   );
 };
-
-const InputTextNoBorderTop: React.FC<InputText> = (props) => {
-  const { label, icon, iconRight } = props;
+const InputTextNoBorderTopBot: React.FC<InputText> = (props) => {
+  const { label, icon, iconRight, inputPos } = props;
   const [text, setText] = useState<null | string>('');
   return (
     <div className='input-no-border-field'>
-      <label>{label}</label>
+      {inputPos === 'top' && <label>{label}</label>}
       <form className='input-no-border-field'>
         {!iconRight && <div className='icon-login'>{icon}</div>}
         <input className='input-no-border' type='text' onChange={(e) => setText(e.target.value)} />
         {iconRight && <div className='icon-login'>{icon}</div>}
       </form>
+      {inputPos === 'bottom' && <label>{label}</label>}
     </div>
   );
 };
-
-const InputTextNoBorderBottom: React.FC<InputText> = (props) => {
-  const { label, icon, iconRight } = props;
-  const [text, setText] = useState<null | string>('');
-  return (
-    <div className='input-no-border-field'>
-      <form className='input-no-border-field'>
-        {!iconRight && <div className='icon-login'>{icon}</div>}
-        <input className='input-no-border' type='text' onChange={(e) => setText(e.target.value)} />
-        {iconRight && <div className='icon-login'>{icon}</div>}
-      </form>
-      <label>{label}</label>
-    </div>
-  );
-};
-
-const InputTextNoBorderLeft: React.FC<InputText> = (props) => {
-  const { label, icon, iconRight } = props;
+const InputTextNoBorderLeftRight: React.FC<InputText> = (props) => {
+  const { label, icon, iconRight, inputPos } = props;
   const [text, setText] = useState<null | string>('');
   return (
     <React.Fragment>
       <div className='input-no-border-inline-field'>
-        <label className='inline-input'>{label}</label>
+        {inputPos === 'left' && <label className='inline-input'>{label}</label>}
         <form className='input-no-border-inline-field'>
           {!iconRight && <div className='icon-login'>{icon}</div>}
           <input className='input-no-border' type='text' onChange={(e) => setText(e.target.value)} />
           {iconRight && <div className='icon-login'>{icon}</div>}
         </form>
-      </div>
-    </React.Fragment>
-  );
-};
-
-const InputTextNoBorderRight: React.FC<InputText> = (props) => {
-  const { label, icon, iconRight } = props;
-  const [text, setText] = useState<null | string>('');
-  return (
-    <React.Fragment>
-      <div className='input-no-border-inline-field'>
-        <form className='input-no-border-inline-field'>
-          {!iconRight && <div className='icon-login'>{icon}</div>}
-          <input className='input-no-border' type='text' onChange={(e) => setText(e.target.value)} />
-          {iconRight && <div className='icon-login'>{icon}</div>}
-        </form>
-        <label className='inline-input'>{label}</label>
+        {inputPos === 'right' && <label className='inline-input'>{label}</label>}
       </div>
     </React.Fragment>
   );
@@ -115,16 +81,3 @@ export const ButtonComponent: React.FC<{ label: string }> = (val) => {
     </div>
   );
 };
-/*
-const InputTextBorder: React.FC<InputText> = (props) => {
-  const { placeholder, icon } = props;
-  const [text, setText] = useState<null | string>('');
-  return (
-    <div className='input-border'>
-      <input className='input-border' type='text' placeholder={placeholder}
-        onChange={(e) => setText(e.target.value)}>
-      </input>
-      <div className='icon-login '>{icon}</div>
-    </div>
-  );
-};*/
